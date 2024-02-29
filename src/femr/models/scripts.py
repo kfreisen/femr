9825e0e9102f2e1b1084a2d42b9685377a52dad3
Kevin Freisen <kevin@zephyrai.bio>
2024-02-29 13:01:14 -0500

    paths

--- src/femr/models/scripts.py
+++ src/femr/models/scripts.py
@@ -652,7 +652,7 @@ def new_compute_representations() -> None:
 
         database = femr.datasets.PatientDatabase(args.data_path)
 
-        with open(os.path.join(args.model_path, "model", "best"), "rb") as f:
+        with open(os.path.join(args.model_path, "best"), "rb") as f:
             params = pickle.load(f)
 
         params = femr.models.transformer.convert_params(params, dtype=jnp.float16)
@@ -660,7 +660,7 @@ def new_compute_representations() -> None:
         with open(batch_info_path, "rb") as f:
             batch_info = msgpack.load(f, use_list=False)
 
-        with open(os.path.join(args.model_path, "model", "config.msgpack"), "rb") as f:
+        with open(os.path.join(args.model_path, "config.msgpack"), "rb") as f:
             config = msgpack.load(f, use_list=False)
 
         config = hk.data_structures.to_immutable_dict(config)
