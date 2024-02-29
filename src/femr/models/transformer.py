b0cfb214e6a536f1df067ea265389c8c8005d4da
Kevin Freisen <kevin@zephyrai.bio>
2024-02-29 16:51:57 -0500

    fix sig

--- src/femr/models/transformer.py
+++ src/femr/models/transformer.py
@@ -125,7 +125,7 @@ class TransformerBlock(hk.Module):
         if hk.running_init():
             attn = jnp.zeros_like(q)
         else:
-            attn = flash_attention_wrapper(q, k, v, length_mask, self.config["attention_width"])
+            attn = flash_attention_wrapper(q, k, v, self.config["attention_width"])
             # attn = femr.jax.local_attention(q, k, v, length_mask, self.config["attention_width"])
 
         def move_out_of_batch(val):
