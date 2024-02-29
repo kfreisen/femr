c21d0b15eb8893200138aaeba289d1ba43ba6d5d
Kevin Freisen <kevin@zephyrai.bio>
2024-02-29 13:58:06 -0500

    nmo labels

--- src/femr/models/scripts.py
+++ src/femr/models/scripts.py
@@ -641,7 +641,9 @@ def new_compute_representations() -> None:
         batches_path = os.path.join(tmpdir, "task_batches")
 
         command = f"clmbr_create_batches {tmpdir}/task_batches --data_path {args.data_path}"
-        command += f" --task labeled_patients --labeled_patients_path {args.prediction_times_path} --val_start 70"
+        command += " --task labeled_patients "
+        # command += f"--labeled_patients_path {args.prediction_times_path} "
+        command += "--val_start 70"
         command += f" --dictionary_path {args.dictionary_path}"
         if config["transformer"]["is_hierarchical"]:
             command += " --is_hierarchical"
